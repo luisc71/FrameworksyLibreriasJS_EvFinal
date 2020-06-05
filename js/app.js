0b4a2d6eef41d11ff902674e9541b59aa5d83a96
Luis Caicedo <luisc71@hotmail.com>
2020-06-04 19:26:16 -0500

    Actualización - Requerimiento 1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-
-  $(function cambioColor(){
-    $("h1").animate({color: '#dcff0e'}, 1000, function(){
-      $("h1").animate({color: '#ffffff'}, 1000, function(){
-        cambioColor();
-      });
+function cambioColor(){
+  $("h1").animate({color: '#dcff0e'}, 1000, function(){
+    $("h1").animate({color: '#ffffff'}, 1000, function(){
+      cambioColor();
     });
-  })
+  });
+};
 
+$(document).ready(function(){
+  cambioColor();
 });
